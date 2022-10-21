f53060fd931b538c6313cebbc4a05c6d0e4e2146
atulpatare <atul.patare@launchventures.co>
2022-10-21 12:21:39 +0530

    added function to add new gif and increment the count

--- programs/solana-rust/src/lib.rs
+++ programs/solana-rust/src/lib.rs
@@ -6,9 +6,16 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod solanarust {
     use super::*;
+
     pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> Result <()> {
         let base_account = &mut ctx.accounts.base_account;
         base_account.total_gifs = 0;
+        Ok(())
+    }
+
+    pub fn add_gif(ctx: Context<AddGif>) -> Result <()> {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.total_gifs += 1;
         Ok(())
     }
 }
@@ -20,6 +27,12 @@ pub struct StartStuffOff<'info> {
     #[account(mut)]
     pub user: Signer<'info>,
     pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct AddGif<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>, 
 }
 
 #[account]
--- tests/solana-rust.js
+++ tests/solana-rust.js
@@ -24,7 +24,15 @@ const main = async () => {
   console.log('Transaction signature ', tx);
 
   let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
-  console.log('GIF Count ', account.totalGifs.toString())
+  console.log('GIF Count ', account.totalGifs.toString());
+
+  await program.rpc.addGif({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+    },
+  });
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('GIF Count ', account.totalGifs.toString());
 }
 
 const runMain = async () => {
